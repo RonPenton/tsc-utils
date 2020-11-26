d36f4c624638d7aef3d4c989bbd1ac61eddabbd5
Penton, Ron <Ron.Penton@ellucian.com>
2020-11-26 13:22:26 -0500

    Fix bug with fully partial and dates

--- src/fullyPartial.ts
+++ src/fullyPartial.ts
@@ -1,9 +1,10 @@
 import { Primitive } from "@sindresorhus/is";
+import { Null } from "./types";
 
 export type FullyPartial<T> = {
     [P in keyof T]?:
-    T[P] extends (infer U)[] ? FullyPartial<U>[] :
-    T[P] extends Primitive ? T[P] :
-    T[P] extends Date ? Date :
+    NonNullable<T[P]> extends Date ? Date | Null<T[P]> :    // need a null hack here to get it to register right.
+    T[P] extends (infer U)[] ? FullyPartial<U>[] :          // process arrays.
+    T[P] extends Primitive ? T[P] :                         // process primitives.
     FullyPartial<T[P]>;
 };

7ef628d04ef637eade2e16e299ede7b2ff1f3029
rlp2 <ron.penton@ellucian.com>
2023-10-24 17:15:49 -0400

    fix

--- src/recordMap.ts
+++ src/recordMap.ts
@@ -4,11 +4,11 @@
  * @param mapper 
  * @returns 
  */
-export function recordMap<T, U>(record: Record<string, T>, mapper: (value: T, index: number) => U) {
+export function recordMap<T, U>(record: Record<string, T>, mapper: (value: T, key: string, index: number) => U) {
     let output: Record<string, U> = {};
     let idx = 0;
     for (const [key, value] of Object.entries(record)) {
-        output[key] = mapper(value, idx);
+        output[key] = mapper(value, key, idx);
         idx++;
     }
     return output;
@@ -20,11 +20,11 @@ export function recordMap<T, U>(record: Record<string, T>, mapper: (value: T, in
  * @param mapper 
  * @returns 
  */
-export async function recordMapAsync<T, U>(record: Record<string, T>, mapper: (value: T, index: number) => Promise<U>) {
+export async function recordMapAsync<T, U>(record: Record<string, T>, mapper: (value: T, key: string, index: number) => Promise<U>) {
     let output: Record<string, U> = {};
     let idx = 0;
     for (const [key, value] of Object.entries(record)) {
-        output[key] = await mapper(value, idx);
+        output[key] = await mapper(value, key, idx);
         idx++;
     }
     return output;

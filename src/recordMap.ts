5ca1adf050d536318beb450a1f61951dd5c0ff36
rlp2 <ron.penton@ellucian.com>
2023-10-24 16:16:12 -0400

    recordmap async

--- src/recordMap.ts
+++ src/recordMap.ts
@@ -1,8 +1,30 @@
+/**
+ * Maps a record using the transformation function.
+ * @param record 
+ * @param mapper 
+ * @returns 
+ */
 export function recordMap<T, U>(record: Record<string, T>, mapper: (value: T, index: number) => U) {
     let output: Record<string, U> = {};
     let idx = 0;
     for (const [key, value] of Object.entries(record)) {
         output[key] = mapper(value, idx);
+        idx++;
+    }
+    return output;
+}
+
+/**
+ * Maps a record using the transformation function asynchronously.
+ * @param record 
+ * @param mapper 
+ * @returns 
+ */
+export async function recordMapAsync<T, U>(record: Record<string, T>, mapper: (value: T, index: number) => Promise<U>) {
+    let output: Record<string, U> = {};
+    let idx = 0;
+    for (const [key, value] of Object.entries(record)) {
+        output[key] = await mapper(value, idx);
         idx++;
     }
     return output;
--- test/recordMap.ts
+++ test/recordMap.ts
@@ -1,4 +1,4 @@
-import { recordMap } from '../src/recordMap';
+import { recordMap, recordMapAsync } from '../src/recordMap';
 
 describe('records:', () => {
 
@@ -14,11 +14,37 @@ describe('records:', () => {
 
         const mapped = recordMap(record, toString);
 
-        expect(mapped).toStrictEqual({ 
+        expect(mapped).toStrictEqual({
             foo: '1',
             bar: '2',
             baz: '3',
             qux: 'true'
         });
+    });
+
+    test('map async', async () => {
+        const testAsync = (x: number | boolean): Promise<String> => {
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    resolve(String(x) + 'async');
+                }, 10);
+            })
+        }
+
+        const record: Record<string, number | boolean> = {
+            foo: 1,
+            bar: 2,
+            baz: 3,
+            qux: true
+        };
+
+        const mapped = await recordMapAsync(record, testAsync);
+        expect(mapped).toStrictEqual({
+            foo: '1async',
+            bar: '2async',
+            baz: '3async',
+            qux: 'trueasync'
+        });
+
     });
 });
